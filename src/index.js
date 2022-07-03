db5fa684ba355cec043703195cbcd7909b1b4837
Nadi Hameza <nadya1gam@gmail.com>
2022-07-03 22:57:13 +0300

    human readeble number

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const { ToWords } = require('to-words');
-const toWords = new ToWords();
 module.exports = function toReadable(number) {
+    const { ToWords } = require('to-words');
+    const toWords = new ToWords();
     let a = toWords.convert(number);
     let str = a.toLowerCase();
     return str;
